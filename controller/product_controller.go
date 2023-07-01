1b9c2c0f8da325224d3fa1ced1541fa0bd0eaacf
vikbert <segentor@gmail.com>
2023-07-01 20:35:08 +0200

    updates

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"net/http"
 )
 
 type ProductController struct {
@@ -12,21 +13,22 @@ func NewProductController() *ProductController {
 }
 
 func (ctrl *ProductController) Create(c *fiber.Ctx) error {
-	return c.Status(204).JSON("")
+
+	return c.Status(http.StatusCreated).JSON("")
 }
 
 func (ctrl *ProductController) List(c *fiber.Ctx) error {
-	return c.Status(204).JSON("")
+	return c.Status(http.StatusOK).JSON("")
 }
 
 func (ctrl *ProductController) Read(c *fiber.Ctx) error {
-	return c.Status(204).JSON("")
+	return c.Status(http.StatusOK).JSON("")
 }
 
 func (ctrl *ProductController) Update(c *fiber.Ctx) error {
-	return c.Status(204).JSON("")
+	return c.Status(http.StatusOK).JSON("")
 }
 
 func (ctrl *ProductController) Delete(c *fiber.Ctx) error {
-	return c.Status(204).JSON("")
+	return c.Status(http.StatusOK).JSON("")
 }
