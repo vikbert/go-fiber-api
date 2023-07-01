1c4dda544e1fd32c53b059c9e7868e0efac2b75a
vikbert <segentor@gmail.com>
2023-07-01 22:48:03 +0200

    updates

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -87,7 +87,26 @@ func (ctrl *ProductController) Read(c *fiber.Ctx) error {
 }
 
 func (ctrl *ProductController) Update(c *fiber.Ctx) error {
-	return c.Status(http.StatusOK).JSON("")
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		c.Status(http.StatusBadRequest).SendString("ID is not an integer!")
+	}
+
+	var postData ProductData
+	if err := c.BodyParser(&postData); err != nil {
+		c.Status(http.StatusBadRequest).SendString(err.Error())
+	}
+
+	var product model.Product
+	if err := findById(id, &product); err != nil {
+		c.Status(http.StatusNotFound).SendString(err.Error())
+	}
+
+	product.Name = postData.Name
+	product.SerialNumber = postData.SerialNumber
+
+	database.Database.Db.Save(&product)
+	return c.Status(http.StatusOK).JSON(createResponse(product))
 }
 
 func (ctrl *ProductController) Delete(c *fiber.Ctx) error {
